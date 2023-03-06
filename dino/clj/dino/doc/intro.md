7e4656bfd972fb9c41a6d72f54366dd2557b7e1e
Dave Fetterman <fettermania@gmail.com>
2023-03-06 09:58:26 -0800

    Dino Wars: Full source project boilerplate

---
+++ dino/clj/dino/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dino
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ dino/clj/dino/test/dino/core_test.clj
@@ -0,0 +1,7 @@
+(ns dino.core-test
+  (:require [clojure.test :refer :all]
+            [dino.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
